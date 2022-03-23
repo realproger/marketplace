526eafb1bd7fdb6c3e8cfebf4889c4633e19a139
Muntasir17 <mtolbaev2000@gmail.com>
2022-03-23 16:25:18 +0600

    marketplace project elaboration

---
+++ apps/products/admin.py
@@ -0,0 +1,6 @@
+from http.client import ImproperConnectionState
+from django.contrib import admin
+from apps.products.models import Product
+# Register your models here.
+
+admin.site.register(Product)
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #apps
+    'apps.categories', 
+    'apps.products', 
+    'apps.settings',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
+STATICFILES_DIRS= [BASE_DIR / 'static']
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
